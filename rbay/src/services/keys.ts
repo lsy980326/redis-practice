1e0270a5a3d3e506a3d22c95aaeac13f9139a2b4
lsy980326 <cxv963@gmail.com>
2025-03-11 03:34:06 +0900

    feat: 사용자 쿼리 함수 추가
    - getUserByUsername 함수 추가
    - getUserById 함수 추가
    - createUser 함수 추가
    - 사용자 정보를 직렬화 및 역직렬화하는 함수 추가

--- rbay/src/services/keys.ts
+++ rbay/src/services/keys.ts
@@ -1,1 +1,5 @@
+// 페이지 캐시 키를 생성하는 함수
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
+
+// 사용자 키를 생성하는 함수
+export const usersKey = (userId: string) => `users#${userId}`;
--- rbay/src/services/queries/users.ts
+++ rbay/src/services/queries/users.ts
@@ -1,8 +1,37 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { client } from '$services/redis';
+import { usersKey } from '$services/keys';
 
+// 사용자 이름으로 사용자 정보를 가져오는 함수
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+// 사용자 ID로 사용자 정보를 가져오는 함수
+export const getUserById = async (id: string) => {
+    const user = await client.hGetAll(usersKey(id));
+    return deserialize(id, user);
+};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+// 새로운 사용자를 생성하는 함수
+export const createUser = async (attrs: CreateUserAttrs) => {
+    const id = genId();
+    await client.hSet(usersKey(id), serialize(attrs));
+    return id;
+};
+
+// 사용자 정보를 직렬화하는 함수
+const serialize = (user: CreateUserAttrs) => {
+    return{
+        username: user.username,
+        password: user.password,
+    };
+};
+
+// 사용자 정보를 역직렬화하는 함수
+const deserialize = (id: string, user:{[key: string]: string}) => {
+    return{
+        id,
+        username: user.username,
+        password: user.password,
+    };
+}
